438916af406ef4819dd941f275a1b5847c0ed010
CAFSI-SENA <51935005+CAFSI-SENA@users.noreply.github.com>
2020-09-04 12:16:39 -0500

    creacion proyecto

---
+++ CursoLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 5, 7, 3, 5, 9, 8, 12 };
+
+            IEnumerable<int> valores = from n in numeros where n > 3 && n < 8 select n;
+
+            // mostrar los resultados
+            foreach (int num in valores)
+                Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
